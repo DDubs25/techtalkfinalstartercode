81f8eb065792e0d391dfd0ae8fe4b96affef70e5
Derrick <dwong@hs-gc.org>
2017-07-28 16:02:02 +0000

    message

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // JavaScript File
 // JavaScript File
-$( document ).ready(function() {
-
-
-});
+    $("#question1button").click(function(){
+      var math = $("question1").val();
+      var question1buttonAnswer ="eating burgers";
+      if (math === question1Answer){
+          alert ("correct");
+      }else {alert ("Incorrect");
+      }
+    });
